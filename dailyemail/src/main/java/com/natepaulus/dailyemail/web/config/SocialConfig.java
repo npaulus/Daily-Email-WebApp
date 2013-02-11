eb37ccf99cc60823732a590f7593e74ae9380b6f
Nate <nate@nate.com>
2013-02-11 09:20:03 -0500

    Added @PropertySource to bring in the properties file

--- dailyemail/src/main/java/com/natepaulus/dailyemail/web/config/SocialConfig.java
+++ dailyemail/src/main/java/com/natepaulus/dailyemail/web/config/SocialConfig.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.support.ConnectionFactoryRegistry;
@@ -13,6 +14,7 @@ import org.springframework.social.facebook.connect.FacebookConnectionFactory;
  * The Class SocialConfig configures spring social settings.
  */
 @Configuration
+@PropertySource("classpath:socialConfig.properties")
 public class SocialConfig {
 
 	@Resource
